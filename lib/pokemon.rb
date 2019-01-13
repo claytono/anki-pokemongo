564a35826ca51a1f16797a03bd06675110c9b965
Clayton O'Neill <claytono@github.com>
2019-01-13 11:04:33 -0500

    Fix images for Spinda and Unown
    If there are multiple forms, but no separate form id, just use the first
    form found.  This seems to only apply for Spinda and Unown, which are
    identical other than the appearance.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -80,20 +80,24 @@ class Pokemon
   end
 
   def calculate_asset_id(gamemaster)
-    return 0 unless form?
-
     # calculate what the form template id would be if it exists
     tid = format('FORMS_V%<number>04d_POKEMON_%<name>s',
       number: number, name: name.upcase)
     forms = gamemaster.dig(tid, 'formSettings', 'forms')
-    return unless forms
+    return 0 unless forms
 
     # Find the form match if it exists.
-    forms = gamemaster[tid]['formSettings']['forms'].select do |form|
+    forms = gamemaster[tid]['formSettings']['forms']
+    matching_forms = gamemaster[tid]['formSettings']['forms'].select do |form|
       form['form'] == @data['form']
     end
 
-    forms.dig(0, 'assetBundleValue') || 0
+    if matching_forms.empty?
+      # If we didn't find any matches, just try to use the first one found.
+      matching_forms = forms
+    end
+
+    matching_forms.dig(0, 'assetBundleValue') || 0
   end
 
   def type_const_to_string(const)
