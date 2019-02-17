1bb52ae78e53576cae52d544e48a0b46686ecde3
Clayton O'Neill <claytono@github.com>
2019-02-17 16:14:46 -0500

    Don't randomize cloze by default in update script
    When generating CSVs to import as cards, we want to randomize them, but
    the update script is partially used to check if there are any changes,
    and randomzing the types defeats that purpose.

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$BASEDIR"
 
 $BASEDIR/setup.sh
-ruby bin/pokemon2anki.rb
+ruby bin/pokemon2anki.rb --no-randomize-cloze
 shasum assets/* >assets.sha1
 
 git diff
